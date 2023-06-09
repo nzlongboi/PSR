184c6531002e1ffea5fb9693e73990406c2349a0
nzlongboi <116671677+nzlongboi@users.noreply.github.com>
2023-06-09 13:05:46 +1200

    add round counter to count round up until 5, then determine a winner. Add non functional reset butotn

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+const tallycount = document.getElementById('tally');
+const playerData = document.getElementById('playerInput');
+const submitButton = document.querySelector('button.submitButton');
+const roundCountElement = document.getElementById('roundCount');
+let playerWins = 0;
+let computerWins = 0;
+let roundsPlayed = 0;
+submitButton.addEventListener('click', playGame);
+// declare and initialise variables and cosntants
+//variables nmaed, numbers are their data type and their starting values are 0
+
+
 function getComputerChoice() {
     var randomResult = (Math.floor(Math.random() *3) +1);
     var computerChoice;
     const displayComputerChoice = document.getElementById('displayComputerChoice');
 switch (randomResult) {
     case 1:
-        computerChoice = 'paper';
-        displayComputerChoice.innerHTML = 'paper';
+        computerChoice = 'Paper';
+        displayComputerChoice.innerHTML = 'Paper';
         break;
     case 2:
-        computerChoice = 'scissors';
-        displayComputerChoice.innerHTML = 'scissors';
+        computerChoice = 'Scissors';
+        displayComputerChoice.innerHTML = 'Scissors';
         break;
     case 3:
-        computerChoice = 'rock';
-        displayComputerChoice.innerHTML = 'rock';
+        computerChoice = 'Rock';
+        displayComputerChoice.innerHTML = 'Rock';
         break;
     default: 
         computerChoice = 'Unknown';
@@ -21,26 +33,47 @@ switch (randomResult) {
 }
 return computerChoice;
 }  
+// I used math.floor(math.random()) to generate a random number between three 
+// and then switch statements to assign a string to each number
 
-const playerInput = document.getElementById('playerInput');
-const submitButton = document.querySelector('submitButton');
-submitButton.addEventListener('click', playGame);
 
 function playGame() {
 const playGameResult = document.getElementById('playGameResult');
 const computerChoice = getComputerChoice();
-const playerChoice = playerInput.value;
+const playerChoice = playerData.value;
 
 if (playerChoice === computerChoice) {
-    playGameResult.innerHTML = 'Its a tie!';
+    playGameResult.innerHTML = 'This round is a tie';
 } else if (
-    (playerChoice === 'paper' && computerChoice === 'rock') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper') ||
-    (playerChoice === 'rock' && computerChoice === 'scissors')) { 
-     playGameResult.innerHTML = 'You Win!!!';
+    (playerChoice === 'Paper' && computerChoice === 'Rock') ||
+    (playerChoice === 'Scissors' && computerChoice === 'Paper') ||
+    (playerChoice === 'Rock' && computerChoice === 'Scissors')) { 
+    playGameResult.innerHTML = `You win! ${playerChoice} beats ${computerChoice}!`;
+    playerWins++;
 } else { 
-    playGameResult.innerHTML = 'You Lose!!!';
+    playGameResult.innerHTML = `You Lose! ${computerChoice} beats ${playerChoice}!`;
+    computerWins++;
 }
     
-} 
+
+
+roundsPlayed++;
+
+if (roundsPlayed === 5) {
+    if (playerWins > computerWins) {
+        tallycount.innerHTML = 'You win the game!';
+    } else if (computerWins > playerWins) {
+        tallycount.innerHTML = 'You lose :(';
+    } else {
+        tallycount.innerHTML = 'It\s a tie;'
+    }
+
+    playerWins = 0;
+    computerWins = 0;
+    roundsPlayed = 0;
+    submitButton.removeEventListener('click, playRound');
+}
+roundCountElement.innerHTML = `Round: ${roundsPlayed}`;
+
+}
 
